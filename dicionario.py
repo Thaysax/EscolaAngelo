141d7678ed29ff789f73a041532dc632161ddfed
Talita Santos da Silva <162985274+Thaysax@users.noreply.github.com>
2024-04-24 08:07:46 -0300

    Add files via upload

---
+++ dicionario.py
@@ -0,0 +1,28 @@
+listaNomes = []
+
+
+listaNomes.append("Talita")
+listaNomes.append("Sarah")
+listaNomes.append("Bigas")
+
+print(listaNomes)
+print(listaNomes[1])
+
+for nome in listaNomes:
+    print(nome)
+    # DICIONÁRIO
+
+    primeiroDicionario = {}
+
+    primeiroDicionario['nome'] = "Talita"
+    primeiroDicionario['tipo'] = 'aluna'
+    primeiroDicionario['numero'] = 9
+    print(primeiroDicionario)
+    print(primeiroDicionario["tipo"])
+
+    #Anatomia
+    # lista[indice] = Valor
+    # dicionario[chave] = Valor
+
+    
+
